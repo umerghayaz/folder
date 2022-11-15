806a590c71fddec2d7dee1d60bc9c29c6082e94a
Umer <UMERSHAHID667@gmail.com>
2022-11-15 15:01:36 +0500

    First commit

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 import os
 
-import mongoengine
+# import mongoengine
 
 import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 from flask import Flask
-from flask_mongoengine import MongoEngine #ModuleNotFoundError: No module named 'flask_mongoengine' = (venv) C:\flaskmyproject>pip install flask-mongoengine 
+# from flask_mongoengine import MongoEngine #ModuleNotFoundError: No module named 'flask_mongoengine' = (venv) C:\flaskmyproject>pip install flask-mongoengine 
 
 UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__)
 app.secret_key = "umer"
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'crud',
-    'host': 'localhost',
-    'port': 27017
-}
+# app.config['MONGODB_SETTINGS'] = {
+#     'db': 'crud',
+#     'host': 'localhost',
+#     'port': 27017
+# }
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg','png','JPG','JPEG','PNG','svg'}
-db = MongoEngine()
-db.init_app(app)
+# db = MongoEngine()
+# db.init_app(app)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -33,8 +33,8 @@ def upload_form():
     return render_template('index.html')
 
 
-class User(db.Document):
-    profile_pic = db.StringField()
+# class User(db.Document):
+#     profile_pic = db.StringField()
 
 
 @app.route('/', methods=['POST'])
@@ -49,8 +49,8 @@ def upload_image():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        usersave = User( profile_pic=file.filename)
-        usersave.save()
+        # usersave = User( profile_pic=file.filename)
+        # usersave.save()
         print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
         o=url_for('static', filename='uploads/' + filename)
